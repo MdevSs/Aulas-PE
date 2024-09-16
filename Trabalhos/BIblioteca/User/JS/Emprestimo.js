7d47b74ea4b6594e191085e863c1eddd5e0d863c
MdevSs <160423146+MdevSs@users.noreply.github.com>
2024-09-16 10:00:02 -0300

    Delete Trabalhos/BIblioteca directory

--- Trabalhos/BIblioteca/User/JS/Emprestimo.js
+++
@@ -1,9 +0,0 @@
-const $back = document.querySelector('img.back')
-
-function VoltarIndex(){
-    window.location.href = '../Index.htm'
-}
-
-$back.addEventListener('click', function(){
-    VoltarIndex()
-})
